e90dac78a7e9c102966c0ff2ee2c762dfaa6d12a
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-10-23 16:22:59 +1000

    Recognizer stub

---
+++ pyxpp/recognizer/__init__.py
@@ -0,0 +1,6 @@
+# pyxpp/recognizer/recognizer.py
+"""_summary_"""
+
+from .recognizer import Recognizer
+
+__all__ = ("Recognizer",)
---
+++ pyxpp/recognizer/recognizer.py
@@ -0,0 +1,6 @@
+# pyxpp/recognizer/recognizer.py
+"""_summary_"""
+
+
+class Recognizer:
+    pass
