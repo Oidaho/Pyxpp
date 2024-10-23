c8abc90d62c5a095071b8343a5c0f3da5eb9cd7d
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-10-23 11:56:16 +1000

    Switching from OS to Pathlib

--- pyxpp/cli/validate.py
+++ pyxpp/cli/validate.py
@@ -1,12 +1,52 @@
 # pyxpp/cli/validate.py
 """_summary_"""
 
-import argparse
-import os
+from pathlib import Path
+
+from .exceptions import WrongExtensionError, NonExistentPathError
 
 
-def file_exists(file_path):
-    """Проверка существования файла."""
-    if not os.path.isfile(file_path):
-        raise argparse.ArgumentTypeError(f"Файл {file_path} не существует.")
-    return file_path
+def FilePath(input_path: str) -> Path:
+    """Checks the existence of the file at the specified path
+    and ensures it has the .py extension.
+
+    Args:
+        input_path (str): Input path to the file.
+
+    Raises:
+        WrongExtensionError: _description_
+        NonExistentPathError: _description_
+
+    Returns:
+        Path: Pathlib Path Object.
+    """
+    path_obj = Path(input_path)
+    if not path_obj.is_file():
+        raise NonExistentPathError(
+            f"'{path_obj}' is not a file or does not exist.",
+        )
+
+    if path_obj.suffix != ".py":
+        raise WrongExtensionError(
+            f"'{path_obj}' must have the .py extension.",
+        )
+
+    return path_obj
+
+
+def DirectoryPath(output_path: str) -> Path:
+    """Checks the existence of the specified directory.
+    Args:
+        output_path (str): _description_
+
+    Raises:
+        ...
+
+    Returns:
+        Path: Pathlib Path Object.
+    """
+    path_obj = Path(output_path)
+
+    # TODO: Write me
+
+    return path_obj
