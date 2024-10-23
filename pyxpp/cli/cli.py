a409b2a7c88910e6d2cbf298613303efd809fc6d
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-10-23 12:05:46 +1000

    The basic set of arguments

--- pyxpp/cli/cli.py
+++ pyxpp/cli/cli.py
@@ -3,19 +3,34 @@
 
 import argparse
 
-from .validate import file_exists
+from .validate import FilePath, DirectoryPath
 
 
 cli_parser = argparse.ArgumentParser(
     prog="pyxpp",
     description="Translator from a variety of Python language to a variety of C++ language.",
-    usage="pyxpp [-h] -i code.py",
+    usage="%(prog)s ./code.py [options]",
+    epilog=(
+        "This software was created solely for educational purposes.\n"
+        "Some functions may not work as intended.\n"
+        "Regular support for the module is not guaranteed.\n"
+    ),
 )
 
+
 cli_parser.add_argument(
-    "-i",
-    help="*.py input file path",
-    required=True,
-    type=file_exists,
+    "input",
+    help="*.py input file",
     metavar="./code.py",
+    type=FilePath,
+)
+
+
+cli_parser.add_argument(
+    "-o",
+    help="output file path",
+    metavar="./cpp_files/",
+    required=False,
+    type=DirectoryPath,
+    default=".",
 )
